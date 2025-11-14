bf433435857ba08051d5eb9297c707793edd7a9f
Mark Sobkow <mark.sobkow@gmail.com>
2025-11-13 20:11:33 -0600

    2025-11-13 20:10 Finally finished relicensing to Dual GPLv3/Commercial

--- src/main/java/org/msscf/msscf/v2_13/cflib/CFLib/JavaFX/CFNumberEditor.java
+++ src/main/java/org/msscf/msscf/v2_13/cflib/CFLib/JavaFX/CFNumberEditor.java
@@ -1,27 +1,24 @@
 /*
- *	MSS Code Factory CFLib 2.13
+ *	MSS Code Factory CFLib 2.13 JavaFX Support
  *
- *	Copyright (c) 2020 Mark Stephen Sobkow
- *
- *	This file is part of MSS Code Factory.
- *
- *	MSS Code Factory is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU Lesser General Public License as published by
+ *	Copyright (C) 2016-2025 Mark Stephen Sobkow (mailto:mark.sobkow@gmail.com)
+ *	
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
- *
- *	MSS Code Factory is distributed in the hope that it will be useful,
+ *	
+ *	This program is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU Lesser General Public License for more details.
- *
- *	You should have received a copy of the GNU Lesser General Public License
- *	along with MSS Code Factory.  If not, see https://www.gnu.org/licenses/.
- *
- *	Donations to support MSS Code Factory can be made at
- *	https://www.paypal.com/paypalme2/MarkSobkow
- *
- *	Contact Mark Stephen Sobkow at mark.sobkow@gmail.com for commercial licensing.
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
  */
 
 package org.msscf.msscf.v2_13.cflib.CFLib.JavaFX;
--- src/main/java/org/msscf/msscf/v2_13/cflib/CFLib/JavaFX/package-info.java
+++ src/main/java/org/msscf/msscf/v2_13/cflib/CFLib/JavaFX/package-info.java
@@ -1,1 +1,23 @@
+/*
+ *	MSS Code Factory CFLib 2.13 JavaFX Support
+ *
+ *	Copyright (C) 2016-2025 Mark Stephen Sobkow (mailto:mark.sobkow@gmail.com)
+ *	
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
 package org.msscf.msscf.v2_13.cflib.CFLib.JavaFX;
